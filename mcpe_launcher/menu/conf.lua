86a71ff47ce29b7ef84b32640f9d826127d68d2e
ImpressiveStay0231 <impressivestay6355@gmail.com>
2026-09-20 13:14:47 +0200

    Delete mcpe_launcher directory

--- mcpe_launcher/menu/conf.lua
+++
@@ -1,8 +0,0 @@
-function love.conf(t)
-    t.window.title = "MCPE Launcher"
-    t.window.width = 640
-    t.window.height = 480
-    t.window.fullscreen = true
-    t.window.fullscreentype = "desktop"
-    t.console = false
-end
